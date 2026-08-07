bd5a38d28d680c770ba08ccfe8336d9579df4f14
MagaJL <48716048@est.ort.edu.ar>
2026-08-07 08:38:54 -0300

    login

--- alarmed-frontend/src/pages/Login.jsx
+++ alarmed-frontend/src/pages/Login.jsx
@@ -1,5 +1,40 @@
+import Input from "../components/Input";
+import Button from "../components/Button";
+import Logo from "../components/Logo";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
-  return <h1>Login</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="phone">
+
+      <Logo />
+
+      <h1>Iniciar sesión</h1>
+
+      <Input
+        label="Correo electrónico"
+        placeholder="Correo electrónico"
+      />
+
+      <Input
+        label="Contraseña"
+        type="password"
+        placeholder="Contraseña"
+      />
+
+      <Button text="Iniciar sesión" />
+
+      <button
+        className="secondary-link"
+        onClick={() => navigate("/register")}
+      >
+        ¿No tenés una cuenta? Registrate
+      </button>
+
+    </div>
+  );
 }
 
 export default Login;
